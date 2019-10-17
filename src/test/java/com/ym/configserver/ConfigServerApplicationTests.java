2db2e647d2d5d50a405dd521322ca8ae3c667cfd
yongmankim <heaven0223@naver.com>
2019-10-18 00:38:47 +0900

    init

---
+++ src/test/java/com/ym/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
